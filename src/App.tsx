67c0d29a327049c1d9f681de82133c60a48ff769
Vinuk Jithsara <gamingvinuff2022@gmail.com>
2026-01-24 14:27:45 +0530

    Add contactpage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,15 +15,9 @@ function App() {
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
       <Routes>
-        <Route
-          path="/"
-          element={<Home isLoggedIn={isLoggedIn} />}
-        />
-
-        <Route
-          path="/about"
-          element={<About isLoggedIn={isLoggedIn} />}
-        />
+        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route path="/about" element={<About isLoggedIn={isLoggedIn} />} />
+        <Route path="/contact" element={<Contact isLoggedIn={isLoggedIn} />} />
       </Routes>
 
       <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type NavbarProps = {
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -39,22 +41,37 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }: NavbarProps) => {
 
           <div className="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
             <ul className="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1">
+
               <li className="nav-item mx-2">
-                <a href="/" className="nav-link active">Home</a>
+                <Link to="/" className="nav-link active">
+                  Home
+                </Link>
               </li>
+
               <li className="nav-item mx-2">
-                <a href="/about" className="nav-link">About</a>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
               </li>
+
               <li className="nav-item mx-2">
-                <a href="#contact" className="nav-link">Contact</a>
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
               </li>
+
               {isLoggedIn && (
                 <>
                   <li className="nav-item mx-2">
-                    <a href="/dashboard" className="nav-link">Dashboard</a>
+                    <Link to="/dashboard" className="nav-link">
+                      Dashboard
+                    </Link>
                   </li>
+
                   <li className="nav-item mx-2">
-                    <a href="/task" className="nav-link">Task</a>
+                    <Link to="/task" className="nav-link">
+                      Task
+                    </Link>
                   </li>
                 </>
               )}
@@ -64,35 +81,33 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }: NavbarProps) => {
             <div className="d-flex align-items-center gap-3">
               {!isLoggedIn ? (
                 <>
-                  <a
-                    href="#login"
+                  <button
                     className="btn px-3 py-1 rounded-3"
                     style={{ border: "1px solid #00B6B0", color: "#00B6B0" }}
                     onClick={() => setIsLoggedIn(true)}
                   >
                     Login
-                  </a>
+                  </button>
 
-                  <a
-                    href="#signup"
+                  <button
                     className="btn px-3 py-1 rounded-3"
                     style={{ border: "1px solid #00B6B0", color: "#00B6B0" }}
                     onClick={() => setIsLoggedIn(true)}
                   >
                     Sign Up
-                  </a>
+                  </button>
                 </>
               ) : (
-                <a
-                  href="#logout"
+                <button
                   className="btn px-3 py-1 rounded-3"
                   style={{ border: "1px solid #ff6b6b", color: "#ff6b6b" }}
                   onClick={() => setIsLoggedIn(false)}
                 >
                   Logout
-                </a>
+                </button>
               )}
             </div>
+
           </div>
         </div>
       </div>
