33572d1b127d32612f607d909e6c3452f312160a
Vinuk Jithsara <gamingvinuff2022@gmail.com>
2026-05-16 14:33:03 +0530

    rout prob

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,4 @@
+import logo from '/src/assets/logo.jpg'
 type AboutProps = {
   isLoggedIn: boolean;
 };
@@ -15,7 +16,7 @@ const About = ({ isLoggedIn }: AboutProps) => {
       <div className="about-content">
 
         <div className="about-logo-pill">
-          <img src="/src/assets/logo.jpg" alt="Worktrack logo" />
+         <img src={logo} alt="Worktrack2010" />
         </div>
 
         {!isLoggedIn ? (
